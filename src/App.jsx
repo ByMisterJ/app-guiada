429ac1bbe10f1db734e7a0d5ba6278bf42099821
daw2 <josearanda9000@gmail.com>
2024-02-22 13:58:44 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Card from './components/Card'
+import ShowHide from './components/ShowHide'
+import vehicles from './data/vehicle'
+
+function App() {
+  const vehicleList = vehicles.map(v=> {
+    return <Card title={v.name} description={v.description} />
+  })
+
+  return (
+    <div className="App">
+      <h1>hola react</h1>
+      <div className='container'>
+        {vehicleList}
+      </div>
+      
+    </div>
+  )
+}
+  
+export default App
---
+++ src/components/ShowHide.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+function ShowHide() {
+    const [show, setShow] = useState(true)
+
+    const handleClick = (event) => {
+        setShow(!show)
+    }
+  return (
+    <div>
+        <button onClick={handleClick}>{show ? "Hide text" : "Show"}</button>
+        {show && <h2>Hide me</h2>}
+    </div>
+  )
+}
+
+export default ShowHide
